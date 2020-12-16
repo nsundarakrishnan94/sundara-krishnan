565d9b844cb219e8e6f9a3a765c29247c721c81c
sundara krishnan <nsundarakrishnan@gmail.com>
2020-12-16 20:30:40 +0530

    updated1

--- form.php
+++ form.php
@@ -4,15 +4,16 @@ $email = $_POST["email"];
 $mobilenumber = $_POST["mobilenumber"];
 $bodytext = $_POST["bodytext"];
 
+$from    = 'nsundarakrishnan@gmail.com'; 
 $to      = 'nsundarakrishnan@gmail.com';
 $subject = 'Job Hire Request from SundaraKrishnan Portfolio';
 $message = 'Name:'.$name.'Email: '.$email.'Mobile: '.$mobilenumber.'Message: '.$bodytext;
-$replysubject='Sundara Krishnan Profile';
-$replymail= "Thank You".$name. "for visiting my page!!, I will be connecting with you shortly. Have a great day :)";
+$replysubject='Job hire request';
+$replymail= "Thank You ".$name. ", for visiting my profile!!, I will be connecting with you shortly. Have a great day :)";
 $headers = 'From:'.$email. "\r\n" .
     'Reply-To: '.$email. "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-mail($to, $subject, $message, $headers);
+mail($from, $to, $subject, $message, $headers);
 mail($email, $replysubject, $replymail);
 ?> 
